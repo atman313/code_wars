55fc53367a8444136fc3905282692199fa34cee0
atman313 <atmanuyag@gmail.com>
2023-03-09 22:25:22 -0800

    new 8kyu

---
+++ 8kyu/8kyu_RemoveStringSpaces.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// My Solution 
+function noSpace(x){return x.split(' ').join('')}
+
+// or 
+
+const noSpace=x=>x.split(" ").join("");
